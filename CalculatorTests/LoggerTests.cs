78d58a613cbc955a205a8ac85c9b6c2af9e38368
StrWatcher <redmoonloon@outlook.com>
2022-06-25 17:34:05 +1000

    Delete non relevant tests

---
+++ CalculatorTests/LoggerTests.cs
@@ -0,0 +1,28 @@
+using Calculator;
+
+namespace CalculatorTests;
+
+public class Tests
+{
+    private Logger _logger = new();
+    [SetUp]
+    public void Setup()
+    {
+        _logger.Erase();
+    }
+
+    [Test]
+    public void LoggerChangeOperationThanExecuteNextOperation()
+    {
+    }
+
+    [Test]
+    public void LoggerSignOperationInEmptyLogSeveralTimes()
+    {
+    }
+
+    [Test]
+    public void Logger()
+    {
+    }
+}
